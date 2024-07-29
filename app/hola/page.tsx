26968caf653173bbffc8a99cce3ed049542ae57c
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-29 19:44:39 -0400

    1

--- app/hola/page.tsx
+++ app/hola/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Hola</h1>
+      <Image
+      src="/next.svg"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+        /> 
     </main>
   );
 }
