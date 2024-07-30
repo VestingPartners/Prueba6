22849257bde3860b21d6045bd160bc5372c23788
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-30 16:37:41 -0400

    1

--- app/api/start.js
+++ app/api/start.js
@@ -1,10 +1,9 @@
-// Creamos un archivo llamado 'data.js' dentro de la carpeta 'api' en la carpeta 'pages'
-// Este archivo exporta una función anónima que maneja la solicitud HTTP GET para la ruta '/api/data'
-export default (req, res) => {
-    // Establecemos el código de estado de la respuesta HTTP en 200
-    res.statusCode = 200;
-    // Establecemos el tipo de contenido de la respuesta HTTP en 'application/json'
-    res.setHeader("Content-Type", "application/json");
-    // Enviamos como respuesta HTTP un objeto JSON que contiene la propiedad 'estamosEn' con el valor 'Medellín JS'
-    res.end(JSON.stringify({ estamosEn: "Medellín JS" }));
-  };
+
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+  	res.status(200).json({ message: 'This is a GET request' });
+  } else {
+  	res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
