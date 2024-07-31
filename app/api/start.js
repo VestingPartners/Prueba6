6932e094783e33da31ad16f0e93d2218a665deb2
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-31 19:58:34 -0400

    333

--- app/api/start.js
+++ app/api/start.js
@@ -1,9 +1,27 @@
 
+'use client'
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-  	res.status(200).json({ message: 'This is a GET request' });
-  } else {
-  	res.status(405).json({ message: 'Method Not Allowed' });
-  }
+import React from 'react';
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Hola</h1>
+      <Image
+      src="/next.svg"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+        /> 
+    
+    <button type="button" onClick={() => router.push('/')}>
+      Dashboard
+    </button>
+
+    </main>
+  );
 }
