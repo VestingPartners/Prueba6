43242640a30edbe19e0c92a44e6cfb8d7a77d9af
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-31 19:53:04 -0400

    button

--- app/hola/page.tsx
+++ app/hola/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import React from 'react';
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Hola</h1>
@@ -11,6 +16,11 @@ export default function Home() {
       height={500}
       alt="Picture of the author"
         /> 
+    
+    <button type="button" onClick={() => router.push('app/')}>
+      Dashboard
+    </button>
+
     </main>
   );
 }
