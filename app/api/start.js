afa30c1f73f2a0b0eb25b7bb3ca486046b16224c
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-31 20:03:10 -0400

    api

--- app/api/start.js
+++ app/api/start.js
@@ -1,27 +1,3 @@
-
-'use client'
-
-import React from 'react';
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-
-export default function Home() {
-  const router = useRouter()
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Hola</h1>
-      <Image
-      src="/next.svg"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-        /> 
-    
-    <button type="button" onClick={() => router.push('/')}>
-      Dashboard
-    </button>
-
-    </main>
-  );
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' });
 }
