6f913c6bf40845e7887c5c3523fde7225b9d8d70
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-08-04 17:42:53 -0400

    e

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: Request) {
   const cookieStore = cookies()
   const token = cookieStore.get('token')
  
-  return new Response('Hello, Next.js!', {
+return new Response('Hello, Next.js!', {
     status: 200,
   })
 }
--- app/api/start.ts
+++ app/api/start.ts
@@ -1,18 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-type ResponseData = {
-  message: string
+import { Request, Response } from 'express';
+
+export default function prueba1(req: Request, res: Response) {
+
+    res.status(200).json({ name: 'Mensaje de Start' })
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  try {
-    console.log('Request received:', req.method, req.url);
-    res.status(200).json({ message: 'Hello from Next.js!' });
-  } catch (error) {
-    console.error('Error handling request:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-} 
