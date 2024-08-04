a4ec10daab262e0f1de8a4d951ad80a238d85814
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-08-04 17:48:47 -0400

    3

---
+++ app/api/start.js
@@ -0,0 +1,8 @@
+
+
+
+export default function prueba1(req, res) {
+
+    res.status(200).json({ name: 'Mensaje de Start' })
+}
+
--- app/api/start.ts
+++
@@ -1,8 +0,0 @@
-
-import { Request, Response } from 'express';
-
-export default function prueba1(req: Request, res: Response) {
-
-    res.status(200).json({ name: 'Mensaje de Start' })
-}
-
